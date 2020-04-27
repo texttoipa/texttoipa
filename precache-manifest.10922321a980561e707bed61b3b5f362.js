7181c5bc3fd7554e32448301364e17113a7bac42
NguyenAnhTrai <nguyenanhtrai27@gmail.com>
2020-04-27 15:23:17 +0700

    Init V1

---
+++ precache-manifest.10922321a980561e707bed61b3b5f362.js
@@ -0,0 +1,210 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17eb99e2b170d37d5ace3294703db9e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b71289738e40fb0b37d8",
+    "url": "/static/css/main.1c9a5649.chunk.css"
+  },
+  {
+    "revision": "342b2f993e2c24210f98",
+    "url": "/static/js/2.07c0d15a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.07c0d15a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b71289738e40fb0b37d8",
+    "url": "/static/js/main.ae2ac1d1.chunk.js"
+  },
+  {
+    "revision": "48be1c98d8aafa7c8407",
+    "url": "/static/js/runtime-main.5e301c7e.js"
+  },
+  {
+    "revision": "741ea969d4f4f60720f02c1398101986",
+    "url": "/static/media/aɪ.741ea969.mp3"
+  },
+  {
+    "revision": "b9f05ff663c55acc29b45902db7d95fd",
+    "url": "/static/media/aʊ.b9f05ff6.mp3"
+  },
+  {
+    "revision": "fdda062b5a43c9331566f74a0174b474",
+    "url": "/static/media/b.fdda062b.mp3"
+  },
+  {
+    "revision": "117264ef41dd9e758242116f89a24edb",
+    "url": "/static/media/d.117264ef.mp3"
+  },
+  {
+    "revision": "675fc6b9f68e3312fee22a14fe7165e0",
+    "url": "/static/media/dʒ.675fc6b9.mp3"
+  },
+  {
+    "revision": "1ea44e04553a3200585ed885f007c047",
+    "url": "/static/media/e.1ea44e04.mp3"
+  },
+  {
+    "revision": "fc4a0ddab1fb0ba0cce0cd68cb971fee",
+    "url": "/static/media/ei.fc4a0dda.mp3"
+  },
+  {
+    "revision": "5731744b15e43e2e7c6bb5f0bed4b26b",
+    "url": "/static/media/eə.5731744b.mp3"
+  },
+  {
+    "revision": "2393ad1d6588ac585809cbc73d41d52e",
+    "url": "/static/media/f.2393ad1d.mp3"
+  },
+  {
+    "revision": "1cc8275806a57ef0fc9eb633250230f3",
+    "url": "/static/media/g.1cc82758.mp3"
+  },
+  {
+    "revision": "39f093e404318b92ab370a0fdab483fd",
+    "url": "/static/media/h.39f093e4.mp3"
+  },
+  {
+    "revision": "81f7900bb8b1f6b3cdf0ac5f9abe4c81",
+    "url": "/static/media/i1.81f7900b.mp3"
+  },
+  {
+    "revision": "4a18910c27fb6e4f273c66b513601c9d",
+    "url": "/static/media/j.4a18910c.mp3"
+  },
+  {
+    "revision": "856b1ed26d596a83ef827f00356ef0c6",
+    "url": "/static/media/k.856b1ed2.mp3"
+  },
+  {
+    "revision": "1474e0f310df675a014712459f43b12a",
+    "url": "/static/media/l.1474e0f3.mp3"
+  },
+  {
+    "revision": "d2c2aa8e3faefbf3cb27b366dff6974c",
+    "url": "/static/media/m.d2c2aa8e.mp3"
+  },
+  {
+    "revision": "f093ba458d2b048e8cb8ffadb91ea4ce",
+    "url": "/static/media/n.f093ba45.mp3"
+  },
+  {
+    "revision": "bd98a63b7cfc5a55e56218ffb27463db",
+    "url": "/static/media/p.bd98a63b.mp3"
+  },
+  {
+    "revision": "fa3c79af4ca9903747c8f98a1fff6ba0",
+    "url": "/static/media/r.fa3c79af.mp3"
+  },
+  {
+    "revision": "b3b24dc8d96b3013b19f75a232a54c6c",
+    "url": "/static/media/refresh.b3b24dc8.svg"
+  },
+  {
+    "revision": "bdd1ca246859dcdb28643dcb4dcbb5b1",
+    "url": "/static/media/s.bdd1ca24.mp3"
+  },
+  {
+    "revision": "7b4c7a37dfe3f43d6519d1e5fcd390c3",
+    "url": "/static/media/speaker.7b4c7a37.svg"
+  },
+  {
+    "revision": "0cd73c92de1aeb0585ec3cc2a20aa09a",
+    "url": "/static/media/t.0cd73c92.mp3"
+  },
+  {
+    "revision": "bc38cb6d3bfd638b60d2c6fbbfb676a3",
+    "url": "/static/media/tʃ.bc38cb6d.mp3"
+  },
+  {
+    "revision": "b92d38b8c2d81eacd56ae6ed8e14fe73",
+    "url": "/static/media/u1.b92d38b8.mp3"
+  },
+  {
+    "revision": "3f68308a1e6e60dc500641ea4e084d2e",
+    "url": "/static/media/v.3f68308a.mp3"
+  },
+  {
+    "revision": "b8a5f7c5f1d2f15479200ad2bf57ceb9",
+    "url": "/static/media/w.b8a5f7c5.mp3"
+  },
+  {
+    "revision": "351810b81d6af2b34113b19bcfa590a5",
+    "url": "/static/media/z.351810b8.mp3"
+  },
+  {
+    "revision": "51e3cf9fec46a4cb4c9f64be9854745d",
+    "url": "/static/media/æ.51e3cf9f.mp3"
+  },
+  {
+    "revision": "1eb46bed7cd5cbdf7cc798abeffa10d0",
+    "url": "/static/media/ð.1eb46bed.mp3"
+  },
+  {
+    "revision": "35e25a860d6928c7a9cc5a1b26a80e7e",
+    "url": "/static/media/ŋ.35e25a86.mp3"
+  },
+  {
+    "revision": "9b4864edccaf98035e958f8da1e61a9c",
+    "url": "/static/media/ɑ1.9b4864ed.mp3"
+  },
+  {
+    "revision": "423c879870023e1ec816a0d79b5a0667",
+    "url": "/static/media/ɒ.423c8798.mp3"
+  },
+  {
+    "revision": "8badfa19a273a5d1d79b73740cd81cc1",
+    "url": "/static/media/ɔ1.8badfa19.mp3"
+  },
+  {
+    "revision": "9004c0a18873d67dc91d976d304833c4",
+    "url": "/static/media/ɔɪ.9004c0a1.mp3"
+  },
+  {
+    "revision": "32f04f625062360bb855f2b159824d50",
+    "url": "/static/media/ə.32f04f62.mp3"
+  },
+  {
+    "revision": "f4f185db78b9f2470875e0f033eeb6b6",
+    "url": "/static/media/əʊ.f4f185db.mp3"
+  },
+  {
+    "revision": "6fd6f71c50883989a237e53c52ff4079",
+    "url": "/static/media/ɜ1.6fd6f71c.mp3"
+  },
+  {
+    "revision": "f6196f5ed3b367789d2a2a0c8b85cfd1",
+    "url": "/static/media/ɪ.f6196f5e.mp3"
+  },
+  {
+    "revision": "1d457068645e95fb440a6b73dacdd70e",
+    "url": "/static/media/ɪə.1d457068.mp3"
+  },
+  {
+    "revision": "88ecc499d45a9b3d623f87a81f8e04cb",
+    "url": "/static/media/ʃ.88ecc499.mp3"
+  },
+  {
+    "revision": "aeebc07ed7c30df2bdcb2d6be47a8508",
+    "url": "/static/media/ʊ.aeebc07e.mp3"
+  },
+  {
+    "revision": "915ee3a8113e29dfeb8f5bd92b7813bd",
+    "url": "/static/media/ʊə.915ee3a8.mp3"
+  },
+  {
+    "revision": "c806b5e1ea3a25496bfacfae9c9f60d8",
+    "url": "/static/media/ʌ.c806b5e1.mp3"
+  },
+  {
+    "revision": "e7ab836e2cceb58cf30a4c53b9869969",
+    "url": "/static/media/ʒ.e7ab836e.mp3"
+  },
+  {
+    "revision": "f73c7511535af2cf5a324d34e5c778b9",
+    "url": "/static/media/θ.f73c7511.mp3"
+  }
+]);
